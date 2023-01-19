7b0b2f91a536e04dfbd8ca2a250644c3bbb4aed8
sagit_zhx <sagit@foxmail.com>
2023-01-20 02:41:29 +0800

    feat: 使用 useContent 为子组件提供消费

---
+++ src/auth-provider.ts
@@ -0,0 +1,55 @@
+/**
+ * @Description	：auth-provider.ts
+ * @Author     	: sagit_zhx
+ * @Date       	: 2023-01-19 星期四 17:23:35
+ * @FilePath	: jira/src/auth-provider.ts
+ */
+const localStorageKey = "__auth_provider_token";
+const apiUrl = process.env.REACT_APP_API_URL || "";
+
+export const getToken = () => globalThis.localStorage.getItem(localStorageKey);
+
+export const handleUserResponse = ({ user }: { user: User }) => {
+  globalThis.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+export const login = (data: { username: string; password: string }) => {
+  return fetch(`${apiUrl}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res) => {
+    if (res.ok) return handleUserResponse(await res.json());
+
+    return Promise.reject(data);
+  });
+};
+
+export const register = (data: { username: string; password: string }) => {
+  return fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res) => {
+    if (res.ok) return handleUserResponse(await res.json());
+
+    return Promise.reject(data);
+  });
+};
+
+export const logout = async () =>
+  globalThis.localStorage.removeItem(localStorageKey);
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+  token: string;
+};
