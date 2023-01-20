e630536efffc1ff349ba9cf35bcecb2a111f1fac
sagit_zhx <sagit@foxmail.com>
2023-01-20 23:01:14 +0800

    chore: 封装fetch请求工具

---
+++ src/http/index.ts
@@ -0,0 +1,59 @@
+/**
+ * @Description	：index.ts
+ * @Author     	: sagit_zhx
+ * @Date       	: 2023-01-20 星期五 02:43:23
+ * @FilePath	: jira/src/http/index.ts
+ */
+
+import qs from "qs";
+import * as auth from "auth-provider";
+import { useAuth } from "context/auth-context";
+
+const apiUrl = process.env.REACT_APP_API_URL || "";
+
+export const http = async (
+  url: string,
+  { data, token, headers, ...customConfig }: Config = {}
+) => {
+  const config = Object.assign(
+    {
+      method: "GET",
+      headers: {
+        Authorization: token ? `Bearer ${token}` : "",
+        "Content-Type": data ? "application/json" : "",
+      },
+    },
+    customConfig
+  );
+
+  Object.is(config.method.toLocaleUpperCase(), "GET")
+    ? (url += `?${qs.stringify(data)}`)
+    : Object.assign(config, { body: JSON.stringify(data || {}) });
+
+  try {
+    const res = await globalThis.fetch(`${apiUrl}/${url}`, config);
+
+    if (res.status === 401) {
+      await auth.logout();
+      globalThis.location.reload();
+      return Promise.reject({ message: "请重新登录" });
+    }
+
+    const data = res.json();
+    return res.ok ? data : Promise.reject(data);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+
+const useHttp = () => {
+  const { user } = useAuth();
+
+  return ([url, config]: Parameters<typeof http>) =>
+    http(url, { ...config, token: user?.token });
+};
+
+type Config = RequestInit & {
+  token?: string;
+  data?: object;
+};
