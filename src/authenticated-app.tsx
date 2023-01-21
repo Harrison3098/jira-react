8b9cd30b7a282ca4aaaec637282e68270669c950
sagit_zhx <sagit@foxmail.com>
2023-01-21 18:25:55 +0800

    feat: 【grid布局】【grid-template-areas】

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -5,13 +5,55 @@
  * @FilePath	: jira/src/authenticated-app.tsx
  */
 import { useAuth } from "context/auth-context";
+import styled from "@emotion/styled";
+
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
 
   return (
-    <div>
-      已登录
-      <button onClick={logout}>登出</button>
-    </div>
+    <Container>
+      <Header>
+        <button onClick={logout}>登出</button>
+      </Header>
+
+      <Nav>nav</Nav>
+
+      <Main>已登录</Main>
+
+      <Aside>aside</Aside>
+
+      <Footer>footer</Footer>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-gap: 5rem;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+`;
+
+// grid-area 用来给grid子元素起名字
+const [Header, Main, Nav, Aside, Footer] = [
+  styled.header`
+    grid-area: header;
+  `,
+  styled.main`
+    grid-area: main;
+  `,
+  styled.nav`
+    grid-area: nav;
+  `,
+  styled.aside`
+    grid-area: aside;
+  `,
+  styled.footer`
+    grid-area: footer;
+  `,
+];
